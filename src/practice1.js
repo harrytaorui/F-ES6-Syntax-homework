60ee6980197c7c7c0e6d200e379cb0ec6d057e92
ruitao <harrytaorui@gmail.com>
2020-11-09 17:45:12 +0800

    [taorui]:完成任务

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,6 @@
-const parseData
-export { parseData };
+const parseData = (obj) => {
+  return obj.data.reduce((acc, next) =>
+    (acc.push(next.reduce((pre, cur, index) =>
+      (pre[obj.column[index].name] = cur, pre), {})), acc), [])
+}
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
-export { inject };
+const inject = (items, section) => {
+  let result = []
+  items.map((item, index) => {
+    let record = section.find(sect => sect.index === index)
+    if (record) {
+      result.push(record.content)
+    }
+    result.push(item)
+  })
+  return result
+}
+export {inject};
